c30d2bf7f62cbc42db932bbc2dcddfa1f4312890
Arif-hckr1 <160994065+Arif-hckr1@users.noreply.github.com>
2025-03-12 12:42:13 +0400

    Create fish.java

---
+++ fish.java
@@ -0,0 +1,13 @@
+package HappyFamily4;
+
+public class Fish extends Pet {
+    public Fish(String nickname, int age, int trickLevel, String[] habits) {
+        super(nickname, age, trickLevel, habits);
+        setSpecies(Species.FISH);
+    }
+
+    @Override
+    public void respond() {
+        System.out.println("Blub blub!");
+    }
+}
