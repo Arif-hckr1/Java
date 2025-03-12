8b4d1dfa7470459be9fa1b07de7f4b1ca4f77fad
Arif-hckr1 <160994065+Arif-hckr1@users.noreply.github.com>
2025-03-12 12:25:08 +0400

    Update comparator.java

--- comparator.java
+++ comparator.java
@@ -8,6 +8,7 @@ class Player {
     }
 }
 class Checker implements Comparator<Player> {
+    @Override
     public int compare(Player a, Player b) {
         if (a.score != b.score) {
             return b.score - a.score;
