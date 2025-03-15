629b138a77366e1f055b2b6a3881c11f9078e03b
Arif Hajiyev <160994065+Arif-hckr1@users.noreply.github.com>
2025-03-15 22:58:51 +0400

    Update StudentApp.java

--- StudentApp.java
+++ StudentApp.java
@@ -1,4 +1,4 @@
-package az.edu.bhos.lesson12_13;
+package lesson12;
 
 import java.util.Arrays;
 import java.util.Comparator;
@@ -9,8 +9,8 @@ public class StudentApp {
         Student[] students = init();
         System.out.println(Arrays.toString(students));
 
-        Arrays.sort(students, new comparator2());
-        Arrays.sort(students, new comparator3());
+        Arrays.sort(students, new Comparator2());
+        Arrays.sort(students, new Comparator3());
       
         System.out.println(Arrays.toString(students));
 
@@ -20,7 +20,7 @@ public class StudentApp {
     public static Student[] init() {
         return new Student[] {
                 new Student(2L, "Eli", 56, 2012, false),
-                new Student(1L, "Vei", 70, 2006, false),
+                new Student(1L, "Veli", 70, 2006, false),
                 new Student(5L, "Fidan", 99, 2005, true),
         };
     }
