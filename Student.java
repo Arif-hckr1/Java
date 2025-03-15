325a4f2c988e636ccf4dfae96e1831c447da3e3e
Arif Hajiyev <160994065+Arif-hckr1@users.noreply.github.com>
2025-03-15 22:46:34 +0400

    Create Student.java

---
+++ Student.java
@@ -0,0 +1,29 @@
+package lesson12;
+
+public final class Player implements Comparable<Student> {
+
+    public final Long id;
+    public final String name;
+    public final int grade;
+    public final int birthdate;
+    public final boolean isFemale;
+
+    public Player(Long id, String name, int score, boolean isFemale) {
+        this.id = id;
+        this.name = name;
+        this.grade = grade;
+        this.birthdate = birthdate;
+        this.isFemale = isFemale;
+    }
+
+    @Override
+    public String toString() {
+        return "{\"id:%d\",\"name\":\"%s\", \"grade\":\"%d\",  \"birthdate\":\"%d\", \"isFemale\":\"%s\"}"
+                .formatted(id, name, grade, birthdate, isFemale);
+    }
+
+    @Override
+    public int compareTo(Student that) {
+        return Long.compare(this.id, that.id);
+    }
+}
